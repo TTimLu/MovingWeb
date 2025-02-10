03a8bebcb5ba12a070111c57b3881a87b216211a
Tim Lu <Tianlu992@gmail.com>
2025-02-10 02:42:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-var userLang = navigator.language || navigator.userLanguage; 
-  if (userLang.includes('zh')) {
-    window.location.href = 'index_zh.html';
-  } else if (userLang.includes('ko')) {
-    window.location.href = 'index_ko.html';
-  } else if (userLang.includes('ja')) {
-    window.location.href = 'index_jp.html';
-  else {
-    window.location.href = 'index_en.html'; // 默认英语
-  }
-
-
 // 滚动时给导航栏添加阴影
 window.addEventListener('scroll', function() {
   const header = document.querySelector('header');
